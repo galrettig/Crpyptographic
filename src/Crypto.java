1e426ee4d44fc26ab9b6a116f3fd8dd64647f743
Gal Rettig <galrettig@Gals-MacBook-Pro.local>
2017-01-29 13:36:50 +0200

    code fixes

---
+++ src/Crypto.java
@@ -0,0 +1,205 @@
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.security.*;
+import java.security.cert.*;
+import java.security.cert.Certificate;
+import java.util.Random;
+
+
+public class Crypto {
+    private Cipher cipher, decipher;
+    private IvParameterSpec ivParameterSpec;
+    private byte[] aesKey;
+    private SecretKeySpec aesKeySpec;
+
+    public Crypto() throws NoSuchAlgorithmException, NoSuchPaddingException, UnsupportedEncodingException {
+        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        ivParameterSpec = new IvParameterSpec("AAAAAAAAAAAAAAAA".getBytes("UTF-8"));
+    }
+
+    private byte[] decrypt(String algorithm, byte[] encryptedMessage, Key keySpec) throws InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
+        if(algorithm.equals("RSA")){
+            decipher = Cipher.getInstance("RSA");
+            decipher.init(Cipher.DECRYPT_MODE, keySpec);
+        } else {
+            decipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            decipher.init(Cipher.DECRYPT_MODE, keySpec, ivParameterSpec);
+        }
+
+        return decipher.doFinal(encryptedMessage);
+
+    }
+
+    private byte[] encrypt(byte[] message, Key keySpec, String algorithm ) throws InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException {
+        cipher = Cipher.getInstance(algorithm);
+        if(algorithm.equals("RSA")){
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+        } else {
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParameterSpec);
+        }
+
+        return cipher.doFinal(message);
+    }
+
+
+    byte[] makeKey() throws NoSuchAlgorithmException {
+        KeyGenerator keygen = KeyGenerator.getInstance("AES");
+        SecretKey secretKey = keygen.generateKey();
+        aesKey = secretKey.getEncoded();
+        aesKeySpec = new SecretKeySpec(aesKey, "AES");
+        return aesKey;
+
+    }
+
+    public PublicKey get_public_key (String keyStore_location, char[] keyStore_password, String alias) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException {
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        FileInputStream fis = new FileInputStream(keyStore_location);
+        keyStore.load(fis, keyStore_password);
+
+        Certificate public_cert = keyStore.getCertificate(alias);
+        PublicKey public_key = public_cert.getPublicKey();
+        return public_key;
+    }
+
+    public PrivateKey get_private_key(String keyStore_location, char[] keyStore_password, String alias, char[] password) throws IOException, KeyStoreException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException {
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        FileInputStream fis = new FileInputStream(keyStore_location);
+        keyStore.load(fis, keyStore_password);
+        PrivateKey private_key = (PrivateKey)keyStore.getKey(alias, password);
+        return private_key;
+    }
+
+    public byte[] encrypt_key_rsa(byte[] message, PublicKey key) throws BadPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, NoSuchPaddingException, InvalidAlgorithmParameterException {
+        byte[] encrypted_key = encrypt(message, key, "RSA");
+        return encrypted_key;
+    }
+
+    public byte[] decrypt_key_rsa(String configFile, PrivateKey keySpec) throws IOException, NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        byte[] encrypted_aes_key = readKeyFromFile(configFile);
+        byte[] decrypted_aes_key = decrypt("RSA", encrypted_aes_key, keySpec);
+        return decrypted_aes_key;
+
+    }
+
+    byte[] decryptFile(String fileLocation, String configFile, PrivateKey private_key) throws IOException, NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        byte[] aes_key = decrypt_key_rsa(configFile, private_key);
+        byte[] decrypted_file = readFile(fileLocation, aes_key);
+        return decrypted_file;
+
+    }
+
+    void encryptFile(String file, String configFile, byte[] encMessage, PublicKey publicKey, PrivateKey my_private_key, String signature_file) throws InvalidAlgorithmParameterException, InvalidKeyException, IOException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, SignatureException {
+        byte[] hash = sign_file(file, my_private_key);
+        byte[] key_encrypted;
+        FileOutputStream fos;
+        FileOutputStream sign_fos = new FileOutputStream(signature_file);
+
+        sign_fos.write(hash);
+        sign_fos.close();
+
+        writeFile(file,encMessage);
+
+        key_encrypted = encrypt_key_rsa(aesKey, publicKey);
+
+        fos = new FileOutputStream(configFile);
+        fos.write(key_encrypted);
+        fos.close();
+    }
+
+    byte[] readFile(String fileLocation, byte[] key) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+
+        Key aes = new SecretKeySpec(key, "AES");
+        File file = new File(fileLocation);
+        FileInputStream fis = new FileInputStream(file);
+
+        byte[] encrypted_text = new byte[fis.available()];
+
+        fis.read(encrypted_text);
+        fis.close();
+
+        Cipher decrypts = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        decrypts.init(Cipher.DECRYPT_MODE, aes, ivParameterSpec);
+
+        byte[] decText = decrypts.doFinal(encrypted_text);
+        return decText;
+
+    }
+
+
+     void writeFile(String file, byte[] encMessage) throws InvalidAlgorithmParameterException, InvalidKeyException, IOException {
+
+            cipher.init(Cipher.ENCRYPT_MODE, aesKeySpec, ivParameterSpec);
+            CipherOutputStream cip = new CipherOutputStream(new FileOutputStream(file), cipher);
+            cip.write(encMessage);
+            cip.flush();
+            cip.close();
+
+    }
+
+    byte[] readHashFromFile(String fileLocation) throws IOException {
+        FileInputStream fis = new FileInputStream(fileLocation);
+        byte[] hash = new byte[fis.available()];
+        fis.read(hash);
+        fis.close();
+        return hash;
+    }
+
+     byte[] readKeyFromFile(String fileLocation) throws IOException {
+
+            FileInputStream fis = new FileInputStream(fileLocation);
+            byte[] key = new byte[fis.available()];
+            fis.read(key);
+            fis.close();
+            return key;
+
+
+    }
+
+    public boolean verify_signature(String signature_file_location, byte[] decrypted_message, PublicKey public_key) throws IOException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        byte[] encrypted_hash = readHashFromFile(signature_file_location);
+        byte[] message_hash = digest(new ByteArrayInputStream(decrypted_message));
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initVerify(public_key);
+        signature.update(message_hash);
+        return signature.verify(encrypted_hash);
+
+    }
+
+    public byte[] sign_file (String file_to_sign, PrivateKey private_key) throws IOException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        FileInputStream fis = new FileInputStream(file_to_sign);
+        byte[] digested = digest(fis);
+        Signature sign = Signature.getInstance("SHA256withRSA");
+        sign.initSign(private_key);
+        sign.update(digested);
+        fis.close();
+        return sign.sign();
+    }
+
+    private byte[] digest (InputStream in) throws NoSuchAlgorithmException, IOException {
+        byte[] bytes = new byte[1024];
+        int b;
+        MessageDigest message_digest = MessageDigest.getInstance("SHA-256");
+        message_digest.reset();
+
+        while((b = in.read(bytes)) != -1){
+            message_digest.update(bytes, 0, b);
+
+        }
+        return message_digest.digest();
+
+    }
+
+
+    private byte[] generateRandomString(int n) {
+        Random rand = new SecureRandom();
+        byte[] randBytes = new byte[n];
+        rand.nextBytes(randBytes);
+        return randBytes;
+
+    }
+
+
+
+}
---
+++ src/main.java
@@ -0,0 +1,105 @@
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.security.*;
+import java.security.cert.*;
+import java.security.cert.Certificate;
+import java.util.Arrays;
+
+public class main {
+
+
+
+    public static byte[] readFile(String fileLocation){
+        try {
+            File file = new File(fileLocation);
+            FileInputStream fis = new FileInputStream(file);
+            byte[] text = new byte[fis.available()];
+            fis.read(text);
+            fis.close();
+            return text;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
+    }
+
+    public static void writeFile(byte[] buffer, String file){
+
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(buffer);
+            fos.close();
+        } catch (Exception e) {e.printStackTrace();}
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException, NoSuchPaddingException {
+        String mode = args[0];
+        Crypto crypto = new Crypto();
+        crypto.makeKey();
+        if(mode.equals("0")){
+            encrypt(Arrays.copyOfRange(args, 1, args.length),crypto);
+        } else if (mode.equals("1")){
+            decrypt(Arrays.copyOfRange(args,1,args.length),crypto);
+        } else {
+            System.out.println("encrypt : 0 file_to_encrypt config_file key_store_path key_store_password alias my_alias my_password signature_file_location");
+            System.out.println("decrypt : 1 file_to_decrypt config_file key_store_path key_store_password alias alias_password other_alias signature_file_location output_dest");
+        }
+    }
+
+
+
+
+    private static void encrypt(String[] args, Crypto crypto){
+        String file_to_encrypt = args[0],
+                config_file = args[1],
+                key_store_path = args[2],
+                key_store_password = args[3],
+                alias = args[4],
+                my_alias = args[5],
+                my_password = args[6],
+                signature_file = args[7];
+        try {
+
+            byte[] message_to_encrypt = readFile(file_to_encrypt);
+            PublicKey public_key = crypto.get_public_key(key_store_path, key_store_password.toCharArray(), alias);
+            PrivateKey my_private_key = crypto.get_private_key(key_store_path, key_store_password.toCharArray(), my_alias, my_password.toCharArray());
+
+            crypto.encryptFile(file_to_encrypt, config_file, message_to_encrypt, public_key, my_private_key, signature_file);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private static void decrypt(String[] args, Crypto crypto){
+        String file_to_decrypt = args[0],
+                config_file = args[1],
+                key_store_path = args[2],
+                key_store_password = args[3],
+                alias = args[4],
+                alias_password = args[5],
+                other_alias = args[6],
+                signature_file_location = args[7],
+        outFile = args[8];
+
+        try {
+            PrivateKey private_key = crypto.get_private_key(key_store_path, key_store_password.toCharArray(), alias, alias_password.toCharArray());
+            PublicKey his_public_key = crypto.get_public_key(key_store_path, key_store_password.toCharArray(), other_alias);
+            byte[] decrypted_file = crypto.decryptFile(file_to_decrypt, config_file, private_key);
+            boolean verify = crypto.verify_signature(signature_file_location,decrypted_file,his_public_key);
+            String outputString = verify ? "File is verified." : "File isn't verified, signature isn't correct";
+            System.out.println(outputString);
+            if(verify){
+                writeFile(decrypted_file, outFile);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
